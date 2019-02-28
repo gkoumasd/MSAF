b91819290101aa5a5339b70642f8fa37207e6419
gkoumasd <gkoumasjim@gmail.com>
2019-02-28 14:38:49 +0000

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 from models.baseline.lstm_normI import lstm_normI
-from models.baseline.simplecnn import baselineCNN
 
 def setup(opt):
     
@@ -9,12 +8,6 @@ def setup(opt):
         model = lstm_normI(opt)  
     return  model
     
-    if opt.network_type == "simplecnn":
-        model = baselineCNN(opt)  
-    return  model
-    
-    
-	
-	
+
 
 
