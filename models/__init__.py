179f1017aea9b8ca4d6c1ade03c41326aaa317bd
gkoumasd <gkoumasjim@gmail.com>
2019-04-08 13:41:27 +0100

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,18 +1,20 @@
-from models.baseline.lstm_normI import lstm_normI
-from models.baseline.baseline import vgg_cnn
+from models.baseline.image import uni_image
+from models.baseline.text import uni_text
+from models.baseline.multimodal import vgg_cnn
 
 def setup(opt):
     
     print("network type: " + opt.network_type)
     
-    if opt.network_type == "baseline":
-        model = vgg_cnn(opt)  
-    return  model
+    if opt.network_type == "multimodal":
+        model = vgg_cnn(opt) 
     
-    if opt.network_type == "lstm_normI":
-        model = lstm_normI(opt)  
-    return  model
+    if opt.network_type == "text_based":
+        model = uni_text(opt)   
     
-
-
-
+    if opt.network_type == "image_based":
+        model = uni_image(opt) 
+        
+     
+    
+    return  model
